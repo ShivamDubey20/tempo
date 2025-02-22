38cfbb7caa5683c8d9b616b4dc72e056ce79ed69
Shivam Dubey <109363873+ShivamDubey20@users.noreply.github.com>
2025-02-22 12:49:58 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
-  server: { port: 5173 },
+  server: {port:5173},
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  optimizeDeps: {
-    include: ['react-router-dom'],
-  },
-  build: {
-    rollupOptions: {
-      external: ['react-toastify', 'axios', 'lucide-react'],
-    },
-  },
-});
+})
+
